16c3e4cc7aaff120c206d240473c3c0acd62333c
Issam Haidaoui <issamhaidaoui@gmail.com>
2024-04-21 01:17:01 +0200

    refactor:log

--- LeagueClient/utils/log.py
+++ LeagueClient/utils/log.py
@@ -2,15 +2,15 @@ import traceback
 import logging
 import sys
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.info("Logger initialized")
+
 def with_try_except(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            print(f"error: {e}, stack: {traceback.format_exc()}. ")
+            logger.error(f"error: {e}, stack: {traceback.format_exc()}. ")
             return e
     return wrapper
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.info("Logger initialized")
