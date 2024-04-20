bd53a149e0526df6a9d97490626dfdbce6bc9153
Issam Haidaoui <issamhaidaoui@gmail.com>
2024-04-20 17:20:21 +0200

    code refactor

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from LeagueClient import LeagueClient
 # TODO Support MAC
 # ref https://hextechdocs.dev/getting-started-with-the-lcu-api/
 # TODO accept Q & Lock Champion
-# TODO conntect to ws wss://riot:lZjUUsYvJwQSoOrmsZMMjA@127.0.0.1:51061/ wss://riot:remote_token@host:port/
+# TODO conntect to ws wss://riot:<remote_toke>@127.0.0.1:51061/ wss://riot:remote_token@host:port/
 # TODO remove Cache file Mechanism (if the lockfile exists then we check (_get_cmd_args) proccess)
 
 with LeagueClient() as lcu:
